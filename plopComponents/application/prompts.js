80e4c9a07ed28e9d458c9c1f8ce48c0bcfd4f4dd
CJ Cenizal <cj@cenizal.com>
2024-01-10 06:34:13 -0800

    Insert branching question for custom data source or sample data.

--- plopComponents/application/prompts.js
+++ plopComponents/application/prompts.js
@@ -25,39 +25,56 @@ Which type of codebase would you like to create?\n`,
       ]
     });
 
+    const dataSourceAns = await inquirer.prompt({
+      type: "list",
+      name: "dataSource",
+      message:
+        "Want to connect to your own data or use our sample data? Sample data consists of pages scraped from docs.vectara.com.",
+      choices: [
+        { name: "Use my own data", value: "customData" },
+        {
+          name: "Use the Vectara Docs sample data",
+          value: "sampleData"
+        }
+      ]
+    });
+
+    const isCustomData = dataSourceAns.dataSource === "customData";
+
     const appNameAns = await inquirer.prompt({
+      when: () => isCustomData,
       type: "input",
       name: "appName",
-      message: "What do you want to name your application? Just accept all defaults to generate a working example.",
-      default: "Vectara Docs Example"
+      message: "What do you want to name your application?"
     });
 
     const customerIdAns = await inquirer.prompt({
+      when: () => isCustomData,
       type: "input",
       name: "customerId",
-      message: "What's your Vectara Customer ID?",
-      default: "1366999410"
+      message: "What's your Vectara Customer ID?"
     });
 
     const corpusIdAns = await inquirer.prompt({
+      when: () => isCustomData,
       type: "input",
       name: "corpusId",
-      message: "What's the Corpus ID of the corpus that contains your data?",
-      default: "1"
+      message: "What's the Corpus ID of the corpus that contains your data?"
     });
 
     const apiKeyAns = await inquirer.prompt({
+      when: () => isCustomData,
       type: "input",
       name: "apiKey",
-      message: "What's your QueryService API Key? This must have access to the corpus.",
-      default: "zqt_UXrBcnI2UXINZkrv4g1tQPhzj02vfdtqYJIDiA"
+      message: "What's your QueryService API Key? This must have access to the corpus."
     });
 
     const questions = [];
     const haveQuestionsAns = await inquirer.prompt({
+      when: () => isCustomData,
       type: "confirm",
       name: "value",
-      message: "Do you want to suggest questions for your users to try?",
+      message: "Do you want the UI to suggest questions for people to try?",
       default: false
     });
 
@@ -83,28 +100,29 @@ Which type of codebase would you like to create?\n`,
       } while (moreQuestionsAns.value);
     }
 
-    const promptAnswers = {
-      ...appTypeAns,
-      ...customerIdAns,
-      ...corpusIdAns,
-      ...apiKeyAns,
-      ...appNameAns,
-      // Convert app name to kebab case.
-      appDirName: appNameAns.appName.toLowerCase().replace(/[\s_]+/g, "-"),
-      questions
-    };
-
-    promptAnswers.questions = JSON.stringify(
-      promptAnswers.questions.length > 0
-        ? promptAnswers.questions
-        : [
+    return isCustomData
+      ? {
+          appType: appTypeAns.appType,
+          appName: appNameAns.appName,
+          appDirName: appNameAns.appName.toLowerCase().replace(/[\s_]+/g, "-"),
+          customerId: customerIdAns.customerId,
+          corpusId: corpusIdAns.corpusId,
+          apiKey: apiKeyAns.apiKey,
+          questions: JSON.stringify(questions)
+        }
+      : {
+          appType: appTypeAns.appType,
+          appName: "Vectara Docs Example",
+          appDirName: "vectara-docs-example",
+          customerId: "1366999410",
+          corpusId: "1",
+          apiKey: "zqt_UXrBcnI2UXINZkrv4g1tQPhzj02vfdtqYJIDiA",
+          questions: JSON.stringify([
             "How do I enable hybrid search?",
             "How is data encrypted?",
             "What is a textless corpus?",
             "How do I configure OAuth?"
-          ]
-    );
-
-    return promptAnswers;
+          ])
+        };
   }
 };
