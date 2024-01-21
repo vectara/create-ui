042e64787e1ad4e2f927b62013a432395dcb8a85
CJ Cenizal <cj@cenizal.com>
2024-01-21 11:33:00 -0800

    Remove grecaptcha dependency.

--- apps/questionAndAnswer/src/index.tsx
+++ apps/questionAndAnswer/src/index.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-// @ts-expect-error - grecaptcha is a global variable.
-grecaptcha.ready(() => {
-  const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-});
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
